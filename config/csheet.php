a5d526ec6db02e3577272594e2ee93249da15a62
Alexey Mezenin <alexeymezenin@gmail.com>
2016-05-04 11:34:57 +0500

    config added

---
+++ config/csheet.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+	'repository' => 'https://github.com/alexeymezenin/laravel-cheat-sheet/tree/master/',
+	
+	'filenames' => [
+		'Blade%20Templates',
+	],
+
+	'languages' => [
+		'en',
+	]
+
+];
